ea1a1a0f80cc42a9345cb33e47a9b5f8eb086a8d
leonctl <leonchantinglok@gmail.com>
2022-02-05 10:44:16 +0800

    mobile integration (DONE)
    Added fire and look around

--- Game CC/Assets/Creator Kit - FPS/Scripts/System/Controller.cs
+++ Game CC/Assets/Creator Kit - FPS/Scripts/System/Controller.cs
@@ -67,6 +67,14 @@ public class Controller : MonoBehaviour
     private Vector2 walkInput;
     [SerializeField]
     private float speed;
+
+    [SerializeField]
+    private Transform cameraTransform;
+    private Vector2 lookInput;
+    [SerializeField]
+    private float LookSensitivity;
+    private float xRotation = 0f;
+
     //new variable
     private CharacterController characterController;
     #endregion
@@ -82,6 +90,33 @@ public class Controller : MonoBehaviour
             walkInput = Vector2.zero;
         }
     }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            lookInput = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            lookInput = Vector2.zero;
+        }
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            m_Weapons[m_CurrentWeapon].triggerDown = true;
+        }
+        else if (context.canceled)
+        {
+            m_Weapons[m_CurrentWeapon].triggerDown = false;
+        }
+    }
+
+
+
 
     public void OpenMenu(InputAction.CallbackContext context)
     {
@@ -145,15 +180,29 @@ public class Controller : MonoBehaviour
     void Update()
     {
         MovementHandler();
+        LookHandler();
+    }
+
+    private void LookHandler()
+    {
+        float sensitivity = LookSensitivity;
+        float tunedInputX = lookInput.x * sensitivity * Time.deltaTime;
+        float tunedInputY = lookInput.y * sensitivity * Time.deltaTime;
+
+        xRotation -= tunedInputY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * tunedInputX);
     }
 
     //keep as reference
     private void OriginalUpdate()
     {
-        if (CanPause && Input.GetButtonDown("Menu"))
-        {
-            PauseMenu.Instance.Display();
-        }
+        //if (CanPause && Input.GetButtonDown("Menu"))
+        //{
+        //    PauseMenu.Instance.Display();
+        //}
 
         //FullscreenMap.Instance.gameObject.SetActive(Input.GetButton("Map"));
 
@@ -163,128 +212,128 @@ public class Controller : MonoBehaviour
         //we define our own grounded and not use the Character controller one as the character controller can flicker
         //between grounded/not grounded on small step and the like. So we actually make the controller "not grounded" only
         //if the character controller reported not being grounded for at least .5 second;
-        if (!m_CharacterController.isGrounded)
-        {
-            if (m_Grounded)
-            {
-                m_GroundedTimer += Time.deltaTime;
-                if (m_GroundedTimer >= 0.5f)
-                {
-                    loosedGrounding = true;
-                    m_Grounded = false;
-                }
-            }
-        }
-        else
-        {
-            m_GroundedTimer = 0.0f;
-            m_Grounded = true;
-        }
+        //if (!m_CharacterController.isGrounded)
+        //{
+        //    if (m_Grounded)
+        //    {
+        //        m_GroundedTimer += Time.deltaTime;
+        //        if (m_GroundedTimer >= 0.5f)
+        //        {
+        //            loosedGrounding = true;
+        //            m_Grounded = false;
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    m_GroundedTimer = 0.0f;
+        //    m_Grounded = true;
+        //}
 
-        Speed = 0;
-        Vector3 move = Vector3.zero;
-        if (!m_IsPaused && !LockControl)
-        {
-            // Jump 
-            if (m_Grounded && Input.GetButtonDown("Jump"))
-            {
-                m_VerticalSpeed = JumpSpeed;
-                m_Grounded = false;
-                loosedGrounding = true;
-                FootstepPlayer.PlayClip(JumpingAudioCLip, 0.8f, 1.1f);
-            }
+        //Speed = 0;
+        //Vector3 move = Vector3.zero;
+        //if (!m_IsPaused && !LockControl)
+        //{
+        //    // Jump 
+        //    if (m_Grounded && Input.GetButtonDown("Jump"))
+        //    {
+        //        m_VerticalSpeed = JumpSpeed;
+        //        m_Grounded = false;
+        //        loosedGrounding = true;
+        //        FootstepPlayer.PlayClip(JumpingAudioCLip, 0.8f, 1.1f);
+        //    }
 
-            bool running = m_Weapons[m_CurrentWeapon].CurrentState == Weapon.WeaponState.Idle && Input.GetButton("Run");
-            float actualSpeed = running ? RunningSpeed : PlayerSpeed;
+        //    bool running = m_Weapons[m_CurrentWeapon].CurrentState == Weapon.WeaponState.Idle && Input.GetButton("Run");
+        //    float actualSpeed = running ? RunningSpeed : PlayerSpeed;
 
-            if (loosedGrounding)
-            {
-                m_SpeedAtJump = actualSpeed;
-            }
+        //    if (loosedGrounding)
+        //    {
+        //        m_SpeedAtJump = actualSpeed;
+        //    }
 
-            // Move around with WASD
-            move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-            if (move.sqrMagnitude > 1.0f)
-                move.Normalize();
+        //    // Move around with WASD
+        //    move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        //    if (move.sqrMagnitude > 1.0f)
+        //        move.Normalize();
 
-            float usedSpeed = m_Grounded ? actualSpeed : m_SpeedAtJump;
+        //    float usedSpeed = m_Grounded ? actualSpeed : m_SpeedAtJump;
 
-            move = move * usedSpeed * Time.deltaTime;
+        //    move = move * usedSpeed * Time.deltaTime;
 
-            move = transform.TransformDirection(move);
-            m_CharacterController.Move(move);
-
-
+        //    move = transform.TransformDirection(move);
+        //    m_CharacterController.Move(move);
 
 
-            // Turn player
-            float turnPlayer = Input.GetAxis("Mouse X") * MouseSensitivity;
-            m_HorizontalAngle = m_HorizontalAngle + turnPlayer;
 
-            if (m_HorizontalAngle > 360) m_HorizontalAngle -= 360.0f;
-            if (m_HorizontalAngle < 0) m_HorizontalAngle += 360.0f;
 
-            Vector3 currentAngles = transform.localEulerAngles;
-            currentAngles.y = m_HorizontalAngle;
-            transform.localEulerAngles = currentAngles;
+        //    // Turn player
+        //    float turnPlayer = Input.GetAxis("Mouse X") * MouseSensitivity;
+        //    m_HorizontalAngle = m_HorizontalAngle + turnPlayer;
 
-            // Camera look up/down
-            var turnCam = -Input.GetAxis("Mouse Y");
-            turnCam = turnCam * MouseSensitivity;
-            m_VerticalAngle = Mathf.Clamp(turnCam + m_VerticalAngle, -89.0f, 89.0f);
-            currentAngles = CameraPosition.transform.localEulerAngles;
-            currentAngles.x = m_VerticalAngle;
-            CameraPosition.transform.localEulerAngles = currentAngles;
+        //    if (m_HorizontalAngle > 360) m_HorizontalAngle -= 360.0f;
+        //    if (m_HorizontalAngle < 0) m_HorizontalAngle += 360.0f;
+
+        //    Vector3 currentAngles = transform.localEulerAngles;
+        //    currentAngles.y = m_HorizontalAngle;
+        //    transform.localEulerAngles = currentAngles;
+
+        //    // Camera look up/down
+        //    var turnCam = -Input.GetAxis("Mouse Y");
+        //    turnCam = turnCam * MouseSensitivity;
+        //    m_VerticalAngle = Mathf.Clamp(turnCam + m_VerticalAngle, -89.0f, 89.0f);
+        //    currentAngles = CameraPosition.transform.localEulerAngles;
+        //    currentAngles.x = m_VerticalAngle;
+        //    CameraPosition.transform.localEulerAngles = currentAngles;
 
             m_Weapons[m_CurrentWeapon].triggerDown = Input.GetMouseButton(0);
 
-            Speed = move.magnitude / (PlayerSpeed * Time.deltaTime);
+            //Speed = move.magnitude / (PlayerSpeed * Time.deltaTime);
 
-            if (Input.GetButton("Reload"))
-                m_Weapons[m_CurrentWeapon].Reload();
+            //if (Input.GetButton("Reload"))
+            //    m_Weapons[m_CurrentWeapon].Reload();
 
-            if (Input.GetAxis("Mouse ScrollWheel") < 0)
-            {
-                ChangeWeapon(m_CurrentWeapon - 1);
-            }
-            else if (Input.GetAxis("Mouse ScrollWheel") > 0)
-            {
-                ChangeWeapon(m_CurrentWeapon + 1);
-            }
+            //if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            //{
+            //    ChangeWeapon(m_CurrentWeapon - 1);
+            //}
+            //else if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            //{
+            //    ChangeWeapon(m_CurrentWeapon + 1);
+            //}
 
             //Key input to change weapon
 
-            for (int i = 0; i < 10; ++i)
-            {
-                if (Input.GetKeyDown(KeyCode.Alpha0 + i))
-                {
-                    int num = 0;
-                    if (i == 0)
-                        num = 10;
-                    else
-                        num = i - 1;
+            //for (int i = 0; i < 10; ++i)
+            //{
+            //    if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+            //    {
+            //        int num = 0;
+            //        if (i == 0)
+            //            num = 10;
+            //        else
+            //            num = i - 1;
 
-                    if (num < m_Weapons.Count)
-                    {
-                        ChangeWeapon(num);
-                    }
-                }
-            }
-        }
+            //        if (num < m_Weapons.Count)
+            //        {
+            //            ChangeWeapon(num);
+            //        }
+            //    }
+            //}
+        //}
 
         // Fall down / gravity
-        m_VerticalSpeed = m_VerticalSpeed - 10.0f * Time.deltaTime;
-        if (m_VerticalSpeed < -10.0f)
-            m_VerticalSpeed = -10.0f; // max fall speed
-        var verticalMove = new Vector3(0, m_VerticalSpeed * Time.deltaTime, 0);
-        var flag = m_CharacterController.Move(verticalMove);
-        if ((flag & CollisionFlags.Below) != 0)
-            m_VerticalSpeed = 0;
+        //m_VerticalSpeed = m_VerticalSpeed - 10.0f * Time.deltaTime;
+        //if (m_VerticalSpeed < -10.0f)
+        //    m_VerticalSpeed = -10.0f; // max fall speed
+        //var verticalMove = new Vector3(0, m_VerticalSpeed * Time.deltaTime, 0);
+        //var flag = m_CharacterController.Move(verticalMove);
+        //if ((flag & CollisionFlags.Below) != 0)
+        //    m_VerticalSpeed = 0;
 
-        if (!wasGrounded && m_Grounded)
-        {
-            FootstepPlayer.PlayClip(LandingAudioClip, 0.8f, 1.1f);
-        }
+        //if (!wasGrounded && m_Grounded)
+        //{
+        //    FootstepPlayer.PlayClip(LandingAudioClip, 0.8f, 1.1f);
+        //}
     }
 
     void PickupWeapon(Weapon prefab)
