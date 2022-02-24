5bb450e165bff209fbd53d4a257ec2ebfb06864f
leonctl <leonchantinglok@gmail.com>
2022-02-24 10:49:10 +0800

    add choose weapon UI logic

---
+++ Game CC/Assets/Scripts/ChooseWeaponManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseWeaponManager : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField]
+    private Image weaponField;
+
+    [SerializeField]
+    private Sprite[] weapons;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TogglePanel(bool show)
+    {
+        animator.SetBool("expand",show);
+    }
+
+    public void ShowWeapon(int weaponIndex)
+    {
+        TogglePanel(false);
+        weaponField.sprite = weapons[weaponIndex];
+    }
+
+
+}
